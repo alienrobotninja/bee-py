73191d33e99d1dbbca5f1d8c1ec66e37f9eff0bd
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2023-11-22 01:46:27 +0000

    21 Nov update. minor change

--- tests/integration/chunk/test_soc.py
+++ tests/integration/chunk/test_soc.py
@@ -5,7 +5,7 @@ from bee_py.utils.hex import bytes_to_hex
 
 # waiting for getPostageBatch
 def test_upload_single_owner_chunk(
-    signer, payload, soc_hash, bee_ky_options, bee_debug_url, try_delete_chunk_from_local_storage
+    signer, payload, soc_hash, bee_ky_options, get_postage_batch, try_delete_chunk_from_local_storage
 ):
     identifier = bytes([0] * 32)
 
@@ -15,6 +15,6 @@ def test_upload_single_owner_chunk(
 
     assert try_delete_chunk_from_local_storage(soc_hash)
 
-    response = upload_single_owner_chunk(bee_ky_options, soc, bee_debug_url)
+    response = upload_single_owner_chunk(bee_ky_options, soc, get_postage_batch("bee_debug_url"))
 
     assert soc_address == response
